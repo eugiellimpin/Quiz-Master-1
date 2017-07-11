be6ea6a2e401274a76e96e1824f5a011e6cfe629
Ryan Ermita <ryan.ermita@infoshiftinc.com>
2017-07-11 10:21:29 +0800

    Convert number to words

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,11 +1,30 @@
 class Question < ActiveRecord::Base
 
 
-  def validate_answer(answer)
-    if answer == self.answer
+  def validate_answer answer
+    submitted_answer = Question.number_to_words(answer)
+    correct_answer = Question.number_to_words(self.answer)
+    if submitted_answer == correct_answer
       return true
     end
 
     return false
   end
+
+  def self.number_to_words answer
+    arr = answer.split(' ')
+    number_pattern = /\A[-+]?[0-9]+\z/
+    words = []
+
+    arr.each do |a|
+     is_number = number_pattern.match(a)
+     if not is_number.nil?
+       a = a.to_i.humanize
+     end
+     words.push(a)
+    end
+    
+    return words.join(' ')
+  end
+
 end
